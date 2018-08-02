e9498dfcfe30deda695e938042f2d1875f38e5f4
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-02 13:39:37 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 char ch = 'e';
+		 {
+		     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+                                    System.out.println("Vowel");
+                              else if(ch>='a'&&ch<='z')
+                                    System.out.println("Consonant");
+	                  else
+		                 System.out.println("invalid");
+		 }
+	}
+}
+	
