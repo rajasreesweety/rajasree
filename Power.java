2bde5e789b0f83916c15f8760274804f42b3db82
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-02 15:29:35 +0530

    Update Power.java

--- Power.java
+++ Power.java
@@ -3,13 +3,16 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
+
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 
-        int base = 3, exponent = 4;
+        int base,exponent;
+		Scanner s=new Scanner(System.in);
+		base=s.nextInt();
+		exponent=s.nextInt();
 
         long result = 1;
 
