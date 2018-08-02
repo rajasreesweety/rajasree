d998415770d32c6c65c6ede1d5f9f107493dffb0
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-02 14:51:50 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int n,i;
+	 Scanner sc=new Scanner(System.in);
+	 n=sc.nextInt();
+	 for ( i = 1; i <= n; i++) 
+	 { 
+	 	System.out.println("Hello\n"); 
+	 	
+	 }
+	}
+}
