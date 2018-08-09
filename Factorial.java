32516477368b280e27c143a70b3947fa8284f4fe
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-09 10:55:30 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int i,fact=1,n;  
+     Scanner sc=new Scanner(System.in);
+     n=sc.nextInt();
+  for(i=1;i<=n;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println(fact);    
+ 
+}
+}
