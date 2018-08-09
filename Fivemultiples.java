2f3e42adf35847fe5d197b5dfe1b9741dd9b16c6
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-09 10:59:32 +0530

    Create Fivemultiples.java

---
+++ Fivemultiples.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int n, i;
+ 
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+ 
+    for(i=1; i<=5; ++i)
+    {
+  System.out.println(n*i);
+  System.out.println("\t");
+ }  
+ 
+	}
+}
