66740efb33e1d6b700f5446c9f85dfb8a297c85d
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-09 11:38:09 +0530

    Create Time29.java

---
+++ Time29.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner in = new Scanner(System.in);
+        int minutes = in.nextInt(); 
+        int p1 = minutes % 60;
+        int p2 = minutes / 60;
+        System.out.print( p2 + " " + p1);
+ 
+	}
+}
