8ab02b4d06505b6c85ef4ff8e6cbe2dd002bd28c
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-10 11:58:30 +0530

    Create String41.java

---
+++ String41.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner ob=new Scanner(System.in);
+		String name=ob.next();
+		int n=ob.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(name);
+		}
+ 
+	}
+}
