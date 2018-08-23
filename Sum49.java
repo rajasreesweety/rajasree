f45dfcb1f5e05a5a3dc4581d7d2083ad008c90f3
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-23 10:22:30 +0530

    Create Sum49.java

---
+++ Sum49.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int s1,s2,sum=0;
+		Scanner sc=new Scanner(System.in);
+		s1=sc.nextInt();
+		s2=sc.nextInt();
+		sum=s1+s2;
+		System.out.print(sum);
+ 
+	}
+}
