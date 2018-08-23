32ad0eccded9a5df98e37015631bec6c2cbf98ea
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-23 10:06:43 +0530

    Create Range44.java

---
+++ Range44.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n>1&&n<10)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+}
