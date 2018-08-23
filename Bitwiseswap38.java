9b5b38b118a1f3a5a8c8bb133e131b20115f73c6
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-23 11:00:16 +0530

    Create Bitwiseswap38.java

---
+++ Bitwiseswap38.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{int a,b;
+	   Scanner sc=new Scanner(System.in);
+	   a=sc.nextInt();
+	   b=sc.nextInt();
+	   a=a^b;
+	   b=a^b;
+	   a=a^b;
+	   System.out.print(a+" "+b);
+ 
+	}
+}
