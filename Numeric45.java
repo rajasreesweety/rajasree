7ebf5fc815c97993213b6a9cd90d8a1fc81cfe29
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-23 10:12:26 +0530

    Create Numeric45.java

---
+++ Numeric45.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int count=0,num;
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+		while(num!=0)
+		{
+			num/=10;
+			count++;
+		}
+		System.out.println(count);
+
+	}
+}
