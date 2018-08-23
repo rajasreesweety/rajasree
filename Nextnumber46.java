13d8f1a638d8601144c0383c31e2c2e513fcc2d1
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-23 09:42:44 +0530

    Create Nextnumber46.java

---
+++ Nextnumber46.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int s;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextInt();
+		s=s+1;
+		System.out.print(s);
+ 
+ 
+	}
+}
