4c377736d1df1589963aaa023fb097e34f53f738
Rajasree Muthumula <41725706+rajasreesweety@users.noreply.github.com>
2018-08-29 11:21:15 +0530

    Create Even54.java

---
+++ Even54.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+ 
+int n;
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	if(n%2==0)
+	System.out.println(n);
+	else
+	System.out.println(n-1);
+	}
+}
